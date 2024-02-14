0758736ee9200a88cdd6f41a1a6ecfe0b9d7bd14
Bruna Dantas <b.dantassoares@gmail.com>
2024-02-14 19:32:28 -0300

    Estrutura de teste unitário.

---
+++ tests/UseCases.Test/Offers/CreateOffer/CreateOfferUseCaseTest.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using FluentAssertions;
+using Moq;
+using RocketseatAuction.API.Communication.Requests;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Services;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
+using Xunit;
+
+namespace UseCases.Test.Offers.CreateOffer;
+public class CreateOfferUseCaseTest
+{
+    [Theory]
+    [InlineData (1)]
+    [InlineData (2)]
+    [InlineData (3)]
+    public void Success(int itemId)
+    {
+        var request = new Faker<RequestCreateOfferJson>()
+            .RuleFor(request => request.Price, f => f.Random.Decimal(50, 1000)).Generate();
+
+        var mock = new Mock<IOfferRepository>();
+        var loggedUser = new Mock<ILoggedUser>();
+        loggedUser.Setup(i => i.User()).Returns(new User());
+
+        var useCase = new CreateOfferUseCase(loggedUser.Object, mock.Object);
+
+        var act = () => useCase.Execute(itemId, request);
+        
+        act.Should().NotThrow();
+    }
+}
